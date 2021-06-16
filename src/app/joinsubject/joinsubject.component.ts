1955ac56337929df00e53adc194b93b0cbc23579
tubai7 <78975852+tubai7@users.noreply.github.com>
2021-06-16 17:07:21 +0530

    Join Subject

---
+++ src/app/joinsubject/joinsubject.component.ts
@@ -0,0 +1,13 @@
+import {Component} from '@angular/core';
+import { NgForm } from "@angular/forms";
+import {MatCardModule} from "@angular/material/card";
+import {MatFormFieldModule} from '@angular/material/form-field';
+
+@Component({
+
+  selector: 'join-subject',
+  templateUrl: 'joinsubject.component.html',
+  styleUrls: ['./joinsubject.component.css']
+})
+export class JoinSubjectComponent {}
+
